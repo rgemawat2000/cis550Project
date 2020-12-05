8e0b46977b9a988c59763388d3788a5415605b66
nmaayeh <nmaayeh@seas.upenn.edu>
2020-12-05 20:30:37 +0200

    Update Recommendations

--- finalproject/client/src/components/Recommendations.js
+++ finalproject/client/src/components/Recommendations.js
@@ -12,10 +12,20 @@ export default class Recommendations extends React.Component {
 		// and the list of recommended movies.
 		this.state = {
 			postalCode: "",
-			recValues: []
+			recValues: [],
+			selectedCategory: "",
+			categories: [],
+			minRating: "",
+			areaAverage: "",
+			selectedDelivery: "",
+			selectedService: ""
 		}
 
 		this.handlePostalChange = this.handlePostalChange.bind(this);
+		this.handleCategoryChange = this.handleCategoryChange.bind(this);
+		this.handleRatingChange = this.handleRatingChange.bind(this);
+		this.handleDeliveryChange = this.handleDeliveryChange.bind(this);
+		this.handleServiceChange = this.handleServiceChange.bind(this);
 		this.submitInput = this.submitInput.bind(this);
 	}
 
@@ -25,8 +35,49 @@ export default class Recommendations extends React.Component {
 		});
 	}
 
+	handleCategoryChange(e) {
+		this.setState({
+			selectedCategory: e.target.value
+		});
+	}
+
+	handleRatingChange(e) {
+		this.setState({
+			minRating: e.target.value
+		});
+	}
+
+	handleDeliveryChange(e) {
+		this.setState({
+			selectedDelivery: e.target.value
+		});
+	}
+
+	handleServiceChange(e) {
+		this.setState({
+			selectedService: e.target.value
+		});
+	}
+
+	componentDidMount() {
+		fetch(`http://localhost:8081/categories`, {
+      		method: 'GET' // The type of HTTP request.
+    	})
+    	.then(res => res.json())
+    	.then(recCategoriesList => {
+			let recCategoriesDivs = recCategoriesList.map((category, i) => 
+				<option value={category.category}>{category.category}</option>
+      		);
+      		//This saves our HTML representation of the data into the state, which we can call in our render function
+      		this.setState({
+        		categories: recCategoriesDivs
+      		});
+    	})
+    	.catch(err => console.log(err))
+	}
+
 	submitInput() {
-		fetch(`http://localhost:8081/recommendations/${this.state.postalCode}`, {
+		fetch(`http://localhost:8081/recommendations/${this.state.postalCode}/${this.state.selectedCategory}/${this.state.minRating}/${this.state.selectedDelivery}/${this.state.selectedService}`, {
       		method: 'GET' // The type of HTTP request.
     	})
     	.then(res => res.json())
@@ -37,17 +88,32 @@ export default class Recommendations extends React.Component {
 					<div className="address">{value.address}</div>
 					<div className="rating">{value.rating}</div>
 					<div className="abvavg">{value.abv_avg}</div>
+					<div className="open">{value.open}</div>
         		</div>
-      	);
+			);
       		//This saves our HTML representation of the data into the state, which we can call in our render function
       		this.setState({
-        		recValues: recValuesDivs
+				recValues: recValuesDivs
       		});
     	})
-    	.catch(err => console.log(err))	
+		.catch(err => console.log(err))	
+
+		fetch(`http://localhost:8081/areaaverage/${this.state.postalCode}`, {
+      		method: 'GET' // The type of HTTP request.
+    	})
+    	.then(res => res.json())
+    	.then(recValuesList => {
+			let ratingDivs = recValuesList.map((value, i) => 
+				value.avg_area_rating
+			);
+      		//This saves our HTML representation of the data into the state, which we can call in our render function
+      		this.setState({
+				areaAverage: "Area Average: "+ratingDivs[0]
+      		});
+    	})
+    	.catch(err => console.log(err))
 	}
 
-	
 	render() {
 
 		return (
@@ -60,15 +126,38 @@ export default class Recommendations extends React.Component {
 			    		<br></br>
 			    		<div className="input-container">
 			    			<input type='text' placeholder="Enter Postal Code" value={this.state.postalCode} onChange={this.handlePostalChange} id="postalCode" className="postal-input"/>
-			    			<button id="submitInputBtn" className="submit-btn" onClick={this.submitInput}>Submit</button>
+			    			<div className="dropdown-container">
+			            		<select value={this.state.selectedCategory} onChange={this.handleCategoryChange} className="dropdown" id="categoriesDropdown">
+			            			<option select value> -- Select a Category -- </option>
+			            			{this.state.categories}
+			            		</select>
+			          		</div>
+							<div className="dropdown-container">
+			            		<select value={this.state.selectedDelivery} onChange={this.handleDeliveryChange} className="dropdown" id="deliveryDropdown">
+			            			<option select value> -- Delivery or Takeout -- </option>
+			            			<option value="Yes">Yes</option>
+									<option value="No">No</option>
+			            		</select>
+			          		</div>
+							<div className="dropdown-container">
+			            		<select value={this.state.selectedService} onChange={this.handleServiceChange} className="dropdown" id="servicesDropdown">
+			            			<option select value> -- Virtual Services -- </option>
+			            			<option value="Yes">Yes</option>
+									<option value="No">No</option>
+			            		</select>
+			          		</div>
+							<input type='text' placeholder="Enter Minimum Rating" value={this.state.minRating} onChange={this.handleRatingChange} id="minRating" className="rating-input"/>
+							<button id="submitInputBtn" className="submit-btn" onClick={this.submitInput}>Submit</button>
 			    		</div>
 			    		<div className="header-container">
+							<div className="h6" id="area_average">{this.state.areaAverage}</div>
 			    			<div className="h6">We Recommend ...</div>
 			    			<div className="headers">
 			    				<div className="header"><strong>Name</strong></div>
 			    				<div className="header"><strong>Address</strong></div>
 					            <div className="header"><strong>Rating</strong></div>
-					            <div className="header"><strong>Above Average</strong></div>
+					            <div className="header"><strong>Above Area Average</strong></div>
+								<div className="header"><strong>Open</strong></div>
 			    			</div>
 			    		</div>
 			    		<div className="results-container" id="results">
